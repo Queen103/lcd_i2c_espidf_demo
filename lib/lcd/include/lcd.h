31fd6a499954942290a7290635c236dd046e7db0
Queen103 <quanhne103@gmail.com>
2024-01-31 00:56:08 +0700

    donr display

--- lib/lcd/include/lcd.h
+++ lib/lcd/include/lcd.h
@@ -18,6 +18,7 @@ esp_err_t write_cmd(i2c_dev_t *dev, uint8_t lcd_res_address, uint8_t lcd_cmd);
 esp_err_t turn_all_pixel(i2c_dev_t *dev);
 void cursor_dev(i2c_dev_t *dev,uint8_t x,uint8_t y);
 void lcd_test_pixal(i2c_dev_t *dev);
-void lcd_test(i2c_dev_t *dev);
+void lcd_test(i2c_dev_t *dev, int x,int y);
 void lcd_clear(i2c_dev_t *dev);
+esp_err_t lcd_display(i2c_dev_t *dev , char *str, int x, int y); // x is row (0->18). y is column (0->8)
 
--- main/main.c
+++ main/main.c
@@ -13,12 +13,13 @@ void app_main()
     if(lcd_init(&dev, I2C_NUM_0, GPIO_NUM_21, GPIO_NUM_22) == ESP_OK){
         printf("duoc roi\n");
         lcd_clear(&dev);
-        // lcd_test(&dev);
-        while(1){
-            lcd_test_pixal(&dev);
-            printf("lap\n");
-            vTaskDelay(5); // Chờ 1000 miligiây (1 giây)
-        }
+        lcd_display(&dev,"Duoc chua anh?",0,0);
+        // lcd_test(&dev,1,1);
+        // while(1){
+        //     lcd_test_pixal(&dev);
+        //     printf("lap\n");
+        //     vTaskDelay(5); // Chờ 1000 miligiây (1 giây)
+        // }
         
         printf("fail\n");
     }
